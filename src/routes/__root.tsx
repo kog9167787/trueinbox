7fe40ea3b7dc3e3982c7e311c3633e3ad67610da
Sahil Gupta <guptas3067@gmail.com>
2026-05-02 12:26:07 +0530

    fxing seo

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -8,46 +8,28 @@ import appCss from '../styles.css?url'
 
 import { QueryClientProvider } from '@tanstack/react-query'
 import type { QueryClient } from '@tanstack/react-query'
+import { seo } from '#/lib/utils'
 
 interface MyRouterContext {
   queryClient: QueryClient
 }
 
-const THEME_INIT_SCRIPT = `(function(){try{var stored=window.localStorage.getItem('theme');var mode=(stored==='light'||stored==='dark'||stored==='auto')?stored:'auto';var prefersDark=window.matchMedia('(prefers-color-scheme: dark)').matches;var resolved=mode==='auto'?(prefersDark?'dark':'light'):mode;var root=document.documentElement;root.classList.remove('light','dark');root.classList.add(resolved);if(mode==='auto'){root.removeAttribute('data-theme')}else{root.setAttribute('data-theme',mode)}root.style.colorScheme=resolved;}catch(e){}})();`
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
   head: () => ({
     meta: [
-
-      {
-        property: 'og:image',
-        content: '/image.png',
-      },
-      {
-        property: 'og:image:width',
-        content: '1200',
-      },
-      {
-        property: 'og:image:height',
-        content: '630',
-      },
-      {
-        name: 'twitter:card',
-        content: 'summary_large_image',
-      },
-      {
-        name: 'twitter:image',
-        content: '/image.png',
-      },
+      ...seo({
+        title:
+          "Inboxly",
+        image: `https://www.inboxly.live/image.png`,
+        url: "https://www.inboxly.live",
+      }),
       {
         charSet: 'utf-8',
       },
       {
         name: 'viewport',
         content: 'width=device-width, initial-scale=1',
-      },
-      {
-        title: 'Inboxly',
       },
     ],
     links: [
@@ -64,18 +46,17 @@ function RootDocument({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <script dangerouslySetInnerHTML={{ __html: THEME_INIT_SCRIPT }} />
         <HeadContent />
         <link
           href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400;0,500;1,400;1,500&family=Geist:wght@300;400;500&display=swap"
           rel="stylesheet"
         ></link>
         <script
-  defer
-  data-website-id="69f4d3af0026f9e90737"
-  data-domain="inboxly.live"
-  src="https://www.insightly.live/script.js">
-  </script>
+          defer
+          data-website-id="69f4d3af0026f9e90737"
+          data-domain="inboxly.live"
+          src="https://www.insightly.live/script.js">
+        </script>
       </head>
       <body className="font-sans antialiased [overflow-wrap:anywhere] selection:bg-[rgba(79,184,178,0.24)]">
         <QueryClientProvider client={Route.useRouteContext().queryClient}>
